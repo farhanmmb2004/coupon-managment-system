732f25d8d5380bcf709900dbc1a6ed33d59f9e6f
farhan <farhankhanmmb2004@gmail.com>
2025-03-13 14:15:05 +0530

    fixing oringin

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app=express();
 app.use(cors({
-    origin:'https://coupon-claim.netlify.app/',
+    origin:'http://localhost:5173',
     credentials: true
 }));
 app.use(express.json({limit:"16Kb"}));
