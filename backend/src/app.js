ead79811c9de15ef2eb7f60f79206dfc8dcc3f91
farhan <farhankhanmmb2004@gmail.com>
2025-03-13 14:55:15 +0530

    das

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,16 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app=express();
 app.use(cors({
-    origin:'https://coupon-claim.netlify.app',
+    origin: ['http://localhost:3000', 'https://coupon-claim.netlify.app'],
     credentials: true
 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://coupon-claim.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 app.use(express.json({limit:"16Kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static('public'));
